41974ede966849cb743e450c4bfa1c5cb907e8b9
REX-Hikamaru <hikamaru5971@gmail.com>
2025-03-02 22:12:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
-    console.log("ページ読み込み完了");
-  
-    // ゲームカードクリックアニメーション
-    $(".game-card").click(function () {
-      $(this).addClass("clicked");
-      setTimeout(() => {
-        $(this).removeClass("clicked");
-      }, 300);
-    });
-  
-    // ダークモード切り替え (おまけ機能)
-    $(".dark-mode-toggle").click(function () {
-      $("body").toggleClass("dark-mode");
-    });
+  console.log("ページ読み込み完了");
+
+  $(".game-card").on("click", function () {
+    $(this).addClass("clicked");
+    setTimeout(() => {
+      $(this).removeClass("clicked");
+    }, 300);
   });
-  
+
+  $(".btn").on("click", function () {
+    alert("ゲームページに移動します");
+  });
+});
